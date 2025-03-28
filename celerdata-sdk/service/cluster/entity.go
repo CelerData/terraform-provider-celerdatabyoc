5700c1ef7890f93d9834b5e8ff692e24962710f9
knightzhao <knightzhao@foxmail.com>
2025-03-28 12:01:48 +0800

    [Enhancement] add nlb endpoint

--- celerdata-sdk/service/cluster/entity.go
+++ celerdata-sdk/service/cluster/entity.go
@@ -272,9 +272,11 @@ type IncrStorageSizeResp struct {
 }
 
 type EndpointsInfo struct {
-	NetworkMethod string `json:"network_method" mapstructure:"network_method"`
-	Host          string `json:"host" mapstructure:"host"`
-	Port          int64  `json:"port" mapstructure:"port"`
+	NetworkMethod   string `json:"network_method" mapstructure:"network_method"`
+	Host            string `json:"host" mapstructure:"host"`
+	Port            int64  `json:"port" mapstructure:"port"`
+	NLBEndpoint     string `json:"nlb_endpoint" mapstructure:"nlb_endpoint"`
+	NlbEndpointType string `json:"nlb_endpoint_type" mapstructure:"nlb_endpoint_type"`
 }
 
 type GetClusterEndpointsReq struct {
