cab5f9861ebb26ce595bd7b53daeabca8562dd94
leeLeft <zuo7you.hub@gmail.com>
2025-04-29 10:07:58 +0800

    [Feature] Feature support update init-script (#71)

--- celerdatabyoc/resource_elastic_cluster.go
+++ celerdatabyoc/resource_elastic_cluster.go
@@ -661,7 +661,7 @@ func IsInstanceStore(d *schema.ResourceData) bool {
 }
 
 func resourceElasticClusterUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var immutableFields = []string{"csp", "region", "cluster_name", "compute_node_ebs_disk_number", "default_admin_password", "data_credential_id", "deployment_credential_id", "network_id", "init_scripts", "query_port"}
+	var immutableFields = []string{"csp", "region", "cluster_name", "compute_node_ebs_disk_number", "default_admin_password", "data_credential_id", "deployment_credential_id", "network_id", "query_port"}
 	for _, f := range immutableFields {
 		if d.HasChange(f) && !d.IsNewResource() {
 			return diag.FromErr(fmt.Errorf("the `%s` field is not allowed to be modified", f))
@@ -744,6 +744,45 @@ func resourceElasticClusterUpdate(ctx context.Context, d *schema.ResourceData, m
 		warningDiag := UpsertClusterLdapSslCert(ctx, clusterAPI, d.Id(), sslCerts, true)
 		if warningDiag != nil {
 			return warningDiag
+		}
+	}
+
+	if d.HasChange("resource_tags") && !d.IsNewResource() {
+		_, n := d.GetChange("resource_tags")
+
+		nTags := n.(map[string]interface{})
+		tags := make(map[string]string, len(nTags))
+		for k, v := range nTags {
+			tags[k] = v.(string)
+		}
+		err := clusterAPI.UpdateResourceTags(ctx, &cluster.UpdateResourceTagsReq{
+			ClusterId: clusterID,
+			Tags:      tags,
+		})
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("cluster (%s) failed to update resource tags: %s", d.Id(), err.Error()))
+		}
+	}
+
+	if d.HasChange("init_scripts") && !d.IsNewResource() {
+		_, n := d.GetChange("init_scripts")
+		vL := n.(*schema.Set).List()
+		scripts := make([]*cluster.Script, 0, len(vL))
+		for _, v := range vL {
+			s := v.(map[string]interface{})
+			scripts = append(scripts, &cluster.Script{
+				ScriptPath: s["script_path"].(string),
+				LogsDir:    s["logs_dir"].(string),
+			})
+		}
+		err := clusterAPI.UpdateDeploymentScripts(ctx, &cluster.UpdateDeploymentScriptsReq{
+			ClusterId: clusterID,
+			Scripts:   scripts,
+			Parallel:  d.Get("run_scripts_parallel").(bool),
+			Timeout:   int32(d.Get("run_scripts_timeout").(int)),
+		})
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to update cluster(%s) init-scripts: %s", d.Id(), err.Error()))
 		}
 	}
 
@@ -951,23 +990,6 @@ func resourceElasticClusterUpdate(ctx context.Context, d *schema.ResourceData, m
 		errDiag := UpdateClusterState(ctx, clusterAPI, d.Get("id").(string), o.(string), n.(string))
 		if errDiag != nil {
 			return errDiag
-		}
-	}
-
-	if d.HasChange("resource_tags") && !d.IsNewResource() {
-		_, n := d.GetChange("resource_tags")
-
-		nTags := n.(map[string]interface{})
-		tags := make(map[string]string, len(nTags))
-		for k, v := range nTags {
-			tags[k] = v.(string)
-		}
-		err := clusterAPI.UpdateResourceTags(ctx, &cluster.UpdateResourceTagsReq{
-			ClusterId: clusterID,
-			Tags:      tags,
-		})
-		if err != nil {
-			return diag.FromErr(fmt.Errorf("cluster (%s) failed to update resource tags: %s", d.Id(), err.Error()))
 		}
 	}
 
